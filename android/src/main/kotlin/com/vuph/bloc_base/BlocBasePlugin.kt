9c69da31213094dc0d6d642afba3057611604045
phuongvu <phuongtinhbien12@gmail.com>
2022-09-03 13:07:49 +0700

    .

---
+++ android/src/main/kotlin/com/vuph/bloc_base/BlocBasePlugin.kt
@@ -0,0 +1,39 @@
+package com.vuph.bloc_base
+
+import androidx.annotation.NonNull
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+
+/** BlocBasePlugin */
+class BlocBasePlugin: FlutterPlugin, MethodCallHandler {
+  private lateinit var channel: MethodChannel
+  private lateinit var context: Context
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "android_navigation_mode")
+    context = flutterPluginBinding.applicationContext
+    channel.setMethodCallHandler(this)
+  }
+
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    if (call.method == "get_navigation_mode") {
+      val resourceIdentity = context.resources.getIdentifier(
+        "config_navBarInteractionMode",
+        "integer",
+        "android"
+      )
+      val navMode = context.resources.getInteger(resourceIdentity)
+      result.success(navMode)
+    } else {
+      result.notImplemented()
+    }
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+}
