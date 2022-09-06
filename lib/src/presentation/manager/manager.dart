f117feb9b1881836b8be8c4b42a8a8c34d97a605
Phuong Vu <31031009+phuongtinhbien@users.noreply.github.com>
2022-09-06 16:55:28 +0700

    Update manager.dart

--- lib/src/presentation/manager/manager.dart
+++ lib/src/presentation/manager/manager.dart
@@ -4,3 +4,4 @@ export './dialogs/dialog_manager.dart';
 export './routing/routing.dart';
 export './theme/app_theme.dart';
 export './theme/theme_manager_cubit.dart';
+export './theme/app_media_query.dart';
