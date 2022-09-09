b606fa03ff89f6c51cb79f2a11c1221c3e5b2e82
phuongvu <phuongtinhbien12@gmail.com>
2022-09-09 16:10:06 +0700

    .

--- lib/src/domain/domain.dart
+++ lib/src/domain/domain.dart
@@ -3,3 +3,4 @@ library domain;
 export './entities/use_case_param.dart';
 export './entities/use_case_response.dart';
 export './use_cases/base_use_case.dart';
+export './repositories/base_repository.dart';
