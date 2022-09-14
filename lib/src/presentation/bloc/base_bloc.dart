2b791731c135ad2a4fe33b3baf52933e54a0ec3e
phuongvu <phuongtinhbien12@gmail.com>
2022-09-14 10:39:26 +0700

    mixin bloc with AppLifecycleState, RouteAware

--- lib/src/presentation/bloc/base_bloc.dart
+++ lib/src/presentation/bloc/base_bloc.dart
@@ -1,18 +1,71 @@
 import 'package:bloc_base_core/src/data/base_failure.dart';
 import 'package:copy_with_extension/copy_with_extension.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
+import 'package:meta/meta.dart';
 
 part 'base_bloc.g.dart';
+
 part 'base_event.dart';
+
 part 'base_state.dart';
 
 /// > BaseBloc is an abstract class that extends Bloc and takes two generic types: T
 /// extends BaseEvent and BaseState
 abstract class BaseBloc<T extends BaseEvent, BaseState>
-    extends Bloc<T, BaseState> {
+    extends Bloc<T, BaseState> with WidgetsBindingObserver, RouteAware {
+  late bool _isMounted;
+
+
+  @mustCallSuper
   BaseBloc(BaseState initialState) : super(initialState) {
+    _isMounted = true;
     listenEvent();
+    WidgetsBinding.instance.addObserver(this);
   }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (_isMounted) {
+      switch (state) {
+        case AppLifecycleState.inactive:
+          onInActive();
+          break;
+        case AppLifecycleState.paused:
+          onPaused();
+          break;
+        case AppLifecycleState.resumed:
+          onResumed();
+          break;
+        case AppLifecycleState.detached:
+          onDetached();
+          break;
+      }
+    }
+  }
+
+  @disposeMethod
+  @override
+  Future<void> close() {
+    _isMounted = false;
+    WidgetsBinding.instance.removeObserver(this);
+    return super.close();
+  }
+
+  @visibleForOverriding
   void listenEvent();
+
+  @visibleForOverriding
+  void onInActive() {}
+
+  @visibleForOverriding
+  void onPaused() {}
+
+  @visibleForOverriding
+  void onResumed() {}
+
+  @visibleForOverriding
+  void onDetached() {}
 }
